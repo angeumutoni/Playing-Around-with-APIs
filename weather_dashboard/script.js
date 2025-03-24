63779fc5e722865c414fbfea10a10f707efd8ea7
angeumutoni <a.umutoni1@alustudent.com>
2025-03-24 17:50:04 +0200

    Update script.js

--- weather_dashboard/script.js
+++ weather_dashboard/script.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
 async function getWeather() {
     const searchInput = document.getElementById('search').value;
     if (!searchInput) {
-        alert('Please enter a city, ZIP code, or coordinates.');
+        alert('Please enter a city or country to search.');
         return;
     }
 
