a7879f648306c48116ab3674cca4e752947d7629
angeumutoni <a.umutoni1@alustudent.com>
2025-03-24 17:47:25 +0200

    Update script.js

--- weather_dashboard/script.js
+++ weather_dashboard/script.js
@@ -1,5 +1,4 @@
-// Load API key from config.js
-const apiKey = API_KEY; 
+const apiKey = '194d40f6e033f574606aa6ace66920c6'; // Using the API Key I got from https://openweathermap.org/
 
 // Set background when the page loads
 document.addEventListener("DOMContentLoaded", () => {
@@ -9,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
 async function getWeather() {
     const searchInput = document.getElementById('search').value;
     if (!searchInput) {
-        alert('Please enter a city or country name to search');
+        alert('Please enter a city, ZIP code, or coordinates.');
         return;
     }
 
@@ -34,11 +33,12 @@ function displayWeather(weatherData) {
     
     resultDiv.innerHTML = `
         <br>
-        <h2>${weatherData.name}, ${weatherData.sys.country}</h2>
+        <h2>Currently In ${weatherData.name}, ${weatherData.sys.country}:</h2>
         <br>
-        <p>Temperature: ${weatherData.main.temp}°C</p>
-        <p>Humidity: ${weatherData.main.humidity}%</p>
-        <p>Wind Speed: ${weatherData.wind.speed} m/s</p>
-        <p>Weather: ${weatherData.weather[0].description}</p>
+        <p><b>Temperature:</b> ${weatherData.main.temp}°C</p>
+        <p><b>Humidity:</b> ${weatherData.main.humidity}%</p>
+        <p><b>Wind Speed:</b> ${weatherData.wind.speed} m/s</p>
+        <br>
+        </p><b>Brief Weather Description:</b> ${weatherData.weather[0].description}</p>
     `;
 }
