10c7089374a35b1b2c004f75076cbd827dff886c
angeumutoni <a.umutoni1@alustudent.com>
2025-03-24 17:17:48 +0200

    Securing the API Key

--- weather_dashboard/config.js
+++ weather_dashboard/config.js
@@ -1,1 +1,1 @@
-const API_KEY = 'your_api_key_here'; // Using the API Key I got from https://openweathermap.org/
+const API_KEY = '194d40f6e033f574606aa6ace66920c6'; // Using the API Key I got from https://openweathermap.org/
