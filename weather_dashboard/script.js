85998aeedecaaad3bd4de5899ad199da0025fc26
angeumutoni <a.umutoni1@alustudent.com>
2025-03-24 17:23:58 +0200

    Linking API config file

--- weather_dashboard/script.js
+++ weather_dashboard/script.js
@@ -1,4 +1,5 @@
-const apiKey = '194d40f6e033f574606aa6ace66920c6'; // Using the API Key I got from https://openweathermap.org/
+// Load API key from config.js
+const apiKey = API_KEY; 
 
 // Set background when the page loads
 document.addEventListener("DOMContentLoaded", () => {
@@ -33,12 +34,11 @@ function displayWeather(weatherData) {
     
     resultDiv.innerHTML = `
         <br>
-        <h2>Currently in ${weatherData.name}, ${weatherData.sys.country}:</h2>
+        <h2>${weatherData.name}, ${weatherData.sys.country}</h2>
         <br>
-        <p><b>Temperature:</b> ${weatherData.main.temp}°C</p>
-        <p><b>Humidity:</b> ${weatherData.main.humidity}%</p>
-        <p><b>Wind Speed:</b> ${weatherData.wind.speed} m/s</p>
-        <br>
-        </p><b>Brief Weather Description:</b> ${weatherData.weather[0].description}</p>
+        <p>Temperature: ${weatherData.main.temp}°C</p>
+        <p>Humidity: ${weatherData.main.humidity}%</p>
+        <p>Wind Speed: ${weatherData.wind.speed} m/s</p>
+        <p>Weather: ${weatherData.weather[0].description}</p>
     `;
 }
